396d0ef0c8b9eba132ba24c21a3412a4fe9bf742
shug7626 <isaac.taylor@st-hughs.ox.ac.uk>
2024-10-16 20:33:03 +0100

    Initial controller design

---
+++ uuv_mission/control.py
@@ -0,0 +1,13 @@
+class PDController:
+    def __init__(self, controller, error = 0):
+        self.Kp = controller[0]
+        self.Kd = controller[1]
+        self.prev_error = error
+    
+    def calculate_control(self, error):
+        
+        derivative = (error - self.prev_error)
+        control_out = (self.Kp * error) + (self.Kd * derivative)
+
+        self.prev_error = error
+        return control_out
