34862753d53b86d502d5679bb7bb328ba64bb17d
shug7626 <isaac.taylor@st-hughs.ox.ac.uk>
2024-10-16 13:35:40 +0100

    Successfully added imports

--- uuv_mission/__init__.py
+++ uuv_mission/__init__.py
@@ -1,1 +1,6 @@
-from . import dynamic
+from .dynamic import Submarine
+from .dynamic import ClosedLoop
+from .dynamic import Mission
+from .dynamic import Trajectory
+
+__all__ = {'Submarine', 'ClosedLoop', 'Mission', 'Trajectory'}
